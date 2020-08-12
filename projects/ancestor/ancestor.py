b1ed16ba891410c17eb28bd00a63e98194f156e4
danhamraj7 <ddan_7@hotmail.com>
2020-08-11 20:27:33 -0400

    fixed indentation on graph, wrote plan and added class stack for ancestors.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,16 @@
+# understand by looking at test file
+# child parent relationship.
 
+# plan
+# Graph problem solving
+# Translate the problem
+# Nodes: the people
+# Edge: when a child has a parent.
+# Build the Graph || get neighbors
+
+# what algorithm to use
+# DFS or BFS will work
+# BFS will go to a leaf may be the better choice
+##
 def earliest_ancestor(ancestors, starting_node):
     pass
